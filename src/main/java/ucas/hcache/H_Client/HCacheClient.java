e20b3324c63c68a1b0aab8f78229ec62c77331cd
刘惊坤 <liujkon@gmail.com>
2016-06-10 10:31:50 +0800

    add structure to HcacheClient

--- src/main/java/ucas/hcache/H_Client/HCacheClient.java
+++ src/main/java/ucas/hcache/H_Client/HCacheClient.java
@@ -4,4 +4,18 @@ package ucas.hcache.H_Client;
  * Created by liujingkun on 16/6/10.
  */
 public class HCacheClient {
+    private boolean redis_cache = false;
+    private boolean local_cache = false;
+
+    public HCacheClient(){
+
+    }
+
+    public void put(){
+
+    }
+
+    public String get(){
+        
+    }
 }
