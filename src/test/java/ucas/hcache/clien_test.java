8e70e3ebfa1a7489a6de1bcc7288b4b547da7cea
刘惊坤 <liujkon@gmail.com>
2016-06-10 15:49:41 +0800

    init test

---
+++ src/test/java/ucas/hcache/clien_test.java
@@ -0,0 +1,23 @@
+package ucas.hcache;
+
+import com.ucas.hcache.HController.HController;
+import junit.framework.TestCase;
+
+/**
+ * Created by liujingkun on 16/6/10.
+ */
+public class clien_test extends TestCase{
+
+    HController client = null;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        client = new HController();
+
+    }
+
+    public void test(){
+        System.out.println("abc");
+    }
+}
