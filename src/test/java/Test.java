dbb9237485e20287c0f996c33af1ed5c7759e6c2
刘惊坤 <liujkon@gmail.com>
2016-06-11 12:16:14 +0800

    conf

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Hashtable;
-import java.util.List;
+import com.ucas.hcache.TopCache.Entry;
+
+import java.util.*;
 
 /**
  * Created by sonny on 2016/6/11.
@@ -13,23 +11,43 @@ public class Test {
     {
         final Hashtable<String, Integer> h = new Hashtable<String, Integer>();
         h.put("a", 3);
-        h.put("b", 1);
+        h.put("b", 2);
         h.put("c", 2);
+        h.put("c", 1);
+        h.put("c", 1);
         for (String str : h.keySet())
         {
             System.out.println(str);
         }
         List<String> v = new ArrayList<String>(h.keySet());
-        Collections.sort(v,new Comparator<Object>()
+        Collections.sort(v,new Comparator<String>()
         {
-            public int compare(Object arg0,Object arg1)
+            public int compare(String arg0,String arg1)
             {
                 return h.get(arg1) - (h.get(arg0));
             }
         });
+        int Top_K = v.size();
+        Top_K = (int)(Top_K*0.8);
+        Hashtable<String, Integer> replace = new Hashtable<String, Integer>();
+        List<String> subList = v.subList(0,Top_K);
+        Iterator it = subList.iterator();
+        while(it.hasNext())
+        {
+            String str = (String) it.next();
+            replace.put(str,h.get(str));
+        }
+        h.clear();
         for (String str : v)
         {
             System.out.println(str + " " + h.get(str));
         }
+        h.putAll(replace);
+
+        for (String str : h.keySet())
+        {
+            System.out.println(str);
+        }
+
     }
 }
