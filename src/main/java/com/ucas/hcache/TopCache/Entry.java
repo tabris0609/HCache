b6c9e490f551ea0be91fdabb5d1d6625465783d9
sonnylier <sonny_lier@163.com>
2016-06-11 15:49:59 +0800

    gongzuoliang

--- src/main/java/com/ucas/hcache/TopCache/Entry.java
+++ src/main/java/com/ucas/hcache/TopCache/Entry.java
@@ -15,19 +15,38 @@ public class Entry
         this.cnt =0;
         this.hot =0;
     }
+
+    /**
+     *
+     * @return
+     */
     public  String getKey()
     {
         return key;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getValue()
     {
         return value;
     }
 
+    /**
+     *
+     * @param val
+     */
     public void setValue(String val)
     {
         this.value = val;
     }
+
+    /**
+     *
+     * @param key
+     */
     public void setKey(String key)
     {
         this.key = key;
